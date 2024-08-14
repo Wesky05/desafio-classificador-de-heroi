491bde26ad936346bb046fe439ab0f5b82741e2e
Wesky05 <kevinnyweslley@gmail.com>
2024-08-13 23:25:08 -0300

    desafio lançado

---
+++ index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Morgana"
+let xpHeroi = 20000
+
+if (xpHeroi<1000){
+console.log ("O héroi de nome " + nomeHeroi + " está no nível de ferro")
+}
+else if (1001<= xpHeroi && xpHeroi<2000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de bronze")
+}
+else if (2001<= xpHeroi && xpHeroi<5000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de prata")
+}
+else if (5001<= xpHeroi && xpHeroi<7000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de ouro")
+}
+else if (7001<= xpHeroi && xpHeroi<8000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de platina")
+}
+else if (8001<= xpHeroi && xpHeroi<9000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de ascendente")
+}
+else if (9001<= xpHeroi && xpHeroi<10000){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de imortal")
+}
+else if (xpHeroi>=10001){
+  console.log ("O héroi de nome " + nomeHeroi + " está no nível de radiante")
+}
